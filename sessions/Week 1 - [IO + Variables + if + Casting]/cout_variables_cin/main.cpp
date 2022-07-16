fd398dd20e781d821b6cd5095c067fdd7f0d2f33
Omar Reda <omarredaelsayied@gmail.com>
2022-07-16 10:56:35 +0200

    1 - [Introduction + cout + variables + cin] - P01

---
+++ sessions/Week 1 - [IO + Variables + if + Casting]/cout_variables_cin/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    cout << 10 << ' ' << 20 << ' ' << 30 << endl;
+//    cout << 'O' << endl;
+//
+//    cout << "Omar Reda" << endl << "My name" << endl;
+//    cout << "My name is \nOmar Reda";
+//    cout << "Omar\\Reda\\Elsayed";
+//    // '/r' == endl
+//    // '/n' ~= endl but BUT
+//    // '/n' > endl in Performance
+
+// ===================================================== //
+
+    return 0;
+}
