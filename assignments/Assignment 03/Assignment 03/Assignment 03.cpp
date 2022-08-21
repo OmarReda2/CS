5287531f5db5c9ff95861d0304d02dce01403b8c
omarr <omarredaelsayied@gmail.com>
2022-08-21 23:40:15 +0200

    Assignment 03  all ex

--- assignments/Assignment 03/Assignment 03/Assignment 03.cpp
+++ assignments/Assignment 03/Assignment 03/Assignment 03.cpp
@@ -28,7 +28,6 @@ int main()
 
 
 
-
 	//// 02:
 	/*int length = 10;
 	for (int i = length; i > 0; i--) {
@@ -62,29 +61,170 @@ int main()
 
 
 
+	//// 04:
+	/*int const len = 5;
+	int arr[len] = { 22, 7, 99, 67, 50 };
+
+	int element;
+	int element_index;
+	cout << "Element to search: ";
+	cin >> element;
+
+	for (int i = 0; i < len; i++) {
+		if (arr[i] == element) {
+			element_index = i;
+			break;
+		}
+	}
+	cout << "Element found in index " << element_index;*/
+
+
+
+
+	//// 05:
+	/*int const len = 5;
+	int arr1[len] = { 1, 2, 3, 4, 5 };
+	int arr2[len] = {};
+
+	for (int i = 0; i < len; i++) {
+		arr2[i] = arr1[i];
+	}
+
+	cout << "arr1: ";
+	for (int i = 0; i < len; i++) {
+		cout << arr1[i] << ' ' ;
+	}
+	cout << endl;
+
+	cout << "arr2: ";
+	for (int i = 0; i < len; i++) {
+		cout << arr2[i] << ' ';
+	}*/
+
+
+
+
+	//// 06:
+	int insert_element = 0;
+	int const len = 10;
+	int arr[len] = { 1, 2, 3, 5, 5, 20, 2, 12, 10 };
+	int arr_repeated[len] = {};
+
+	for (int i = 0; i < len; i++) {
+		for (int j = 1; j < i; j++) {
+			if (arr[i] == arr[j]) {
+				arr_repeated[insert_element] = arr[i];
+				insert_element++;
+				break;
+			}
+		}
+	}
+
+
+	cout << "Unique elements is: ";
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len; j++) {
+
+			if (arr[i] == arr_repeated[j]) {
+
+				arr[i] = -1;
+				
+			}
+		}
+		
+		if (arr[i] != -1 || arr[i] == 0)
+			cout << arr[i] << ' ';
+	}
+
+	
+
+
+
+
+
 
 	//// 07:
-	int arr1[5] = { 1, 2, 3, 4, 5 };
+	/*const int arr1_length = 5;
 	int arr1_count = 0;
+	int arr1[arr1_length] = { 1, 2, 3, 4, 5 };
 
-	int arr2[5] = { 6, 7, 8, 9, 10 };
+	const int arr2_length = 5;
 	int arr2_count = 0;
+	int arr2[arr2_length] = { 6, 7, 8, 9, 10,};
 
-	int arr3[10];
-	for (int i = 0;; i++) {
-		while (arr1_count < 5) {
-			arr3[i] = arr1[i];
-			arr1_count++;
-		}
+	const int arr3_length = 30;
+	int arr3_count = 0;
+	int arr3[arr3_length];
 
-		while (arr2_count < 5) {
-			arr3[i] = arr2[i];
-			arr1_count++;
-		}
-		break;
+	while (arr1_count != arr1_length) {
+		arr3[arr3_count] = arr1[arr1_count];
+		arr1_count++;
+		arr3_count++;
 	}
 
-	for (int i = 0; i < 10; i++){
-		cout << arr3[i] << 1;
+	while (arr2_count != arr2_length) {
+		arr3[arr3_count] = arr2[arr2_count];
+		arr2_count++;
+		arr3_count++;
 	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << arr3[i] << endl;
+	}*/
+
+
+
+
+
+
+
+
+	//// Bounce 
+	//// 01:
+	/*int row;
+	cout << "Enter the rows number: ";
+	cin >> row;
+
+	int dash = row - 1;
+	int star = 1;
+
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < dash; j++) {
+			cout << ' ';
+		}
+		dash--;
+
+		for (int k = 0; k < star; k++) {
+			cout << '*';
+		}
+		star += 2;
+		cout << endl;
+	}*/
+
+
+
+
+	//// 02: 
+	/*int row;
+	cout << "Enter the rows number: ";
+	cin >> row;
+	int dash = row - 1;
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < dash; j++) {
+			cout << ' ';
+		}
+		dash--;
+		cout << "******";
+		cout << endl;
+	}*/
+
+
+
+
+
+
+
+
 }
