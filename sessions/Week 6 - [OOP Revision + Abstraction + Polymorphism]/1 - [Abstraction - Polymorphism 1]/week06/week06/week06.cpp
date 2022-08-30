27b79d1bc4148f7445c70cc48bb77e3bd5e924c9
omarr <omarredaelsayied@gmail.com>
2022-08-30 21:18:09 +0200

    week 06 - polymorphism : Function overloading

--- sessions/Week 6 - [OOP Revision + Abstraction + Polymorphism]/1 - [Abstraction - Polymorphism 1]/week06/week06/week06.cpp
+++ sessions/Week 6 - [OOP Revision + Abstraction + Polymorphism]/1 - [Abstraction - Polymorphism 1]/week06/week06/week06.cpp
@@ -36,12 +36,44 @@ using namespace std;
 #pragma endregion
 
 
+#pragma region Polymorphism : Function overloading
+class Add {
+public:
+    static int add(int a, int b) {
+        cout << "...1";
+        return a + b;
+    }
 
+    static int add(int a, int b, int c) {
+        cout << "...2";
+        return a + b + c;
+    }
+
+    static double add(double a, double b) {
+        cout << "...3";
+        return a + b;
+    }
+
+    static double add(int a, double b) {
+        cout << "...4";
+        return a + b;
+    }
+
+    static double add(double a, int b) {
+        cout << "...5";
+        return a + b;
+    }
+};
 
 int main()
 {
-
-    
-
+    cout << "=> " << Add::add(1, 2) << endl;
+    cout << "=> " << Add::add(1, 2, 3) << endl;
+    cout << "=> " << Add::add(1.5, 2.3) << endl;
+    cout << "=> " << Add::add(1, 2.3) << endl;
+    cout << "=> " << Add::add(1.5, 2) << endl;
 }
+#pragma endregion
+
+
 
