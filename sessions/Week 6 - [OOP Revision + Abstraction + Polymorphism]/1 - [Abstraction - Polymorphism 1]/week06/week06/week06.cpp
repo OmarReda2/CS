1a51e0de67f2d86a0b72e43259f60239816b076c
omarr <omarredaelsayied@gmail.com>
2022-08-30 20:40:28 +0200

    week 06 - abstraction

---
+++ sessions/Week 6 - [OOP Revision + Abstraction + Polymorphism]/1 - [Abstraction - Polymorphism 1]/week06/week06/week06.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+#pragma region Abstraction
+//class Person {
+//public:
+//    string name;
+//    virtual void work() = 0;
+//
+//};
+//
+//class Student : public Person {
+//public:
+//    double marks;
+//    void work() {
+//        cout << "I am studying";
+//    }
+//};
+//
+//class Teacher : public Person {
+//public:
+//    double salary;
+//};
+//
+//int main()
+//{
+//
+//    Student std;
+//    std.work();
+//
+//}
+
+#pragma endregion
+
+
+
+
+int main()
+{
+
+    
+
+}
+
