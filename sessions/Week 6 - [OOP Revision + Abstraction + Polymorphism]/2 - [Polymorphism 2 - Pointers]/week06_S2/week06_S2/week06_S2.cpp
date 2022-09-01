4b46f3b8ac3ede896c69238b7bf9fb1080cf4dd8
omarr <omarredaelsayied@gmail.com>
2022-09-01 22:58:12 +0200

    week 06 - pointers

---
+++ sessions/Week 6 - [OOP Revision + Abstraction + Polymorphism]/2 - [Polymorphism 2 - Pointers]/week06_S2/week06_S2/week06_S2.cpp
@@ -0,0 +1,61 @@
+using namespace std;
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+#pragma region Pointers
+	/*int x = 5;
+	int* ptr;
+	ptr = &x;
+	cout << ptr << ' ' << *ptr << ' ' << &ptr;
+
+
+	*ptr = 10;
+	cout << ptr << ' ' << *ptr << ' ' << &ptr;
+
+	*ptr++;
+	cout << ptr << ' ' << *ptr << ' ' << &ptr;*/
+#pragma endregion
+
+
+
+#pragma region Pointers and array
+	/*int arr[3] = { 1, 2, 3 };
+
+	int* ptr = arr;
+	cout << ptr << ' ' << arr << endl;
+	cout << &ptr << ' ' << &arr << endl;
+	cout << *ptr << ' ' << *arr << endl;
+
+	ptr++;
+	cout << *ptr << ' ' << *arr << endl;
+
+	cout << *(arr + 1) << endl;
+	cout << *(arr + 1) + 1 << endl;*/
+#pragma endregion
+
+
+#pragma region Pointers and new
+	//int* ptr;
+	//ptr = new int;
+	//*ptr = 5;
+	//delete ptr;
+
+
+	//int* ptrArr = new int[5];
+	//ptrArr[0] = 1;
+	//ptrArr[1] = 2;
+	//cout << ptr[0];
+	////delete ptrArr; // delete the first address of array
+	//delete[] ptrArr;
+#pragma endregion
+
+
+
+
+
+	
+}
