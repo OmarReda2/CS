f33f725a82c24f4e961a8561d9083e2198e187b4
omarr <omarredaelsayied@gmail.com>
2022-09-02 22:36:41 +0200

    Assignment 03

--- assignments/OOP/Assignment 03 OOP/Assignment 03 OOP/Assignment 03 OOP.cpp
+++ assignments/OOP/Assignment 03 OOP/Assignment 03 OOP/Assignment 03 OOP.cpp
@@ -136,6 +136,32 @@ public:
 
 
 
+// -------- Exception Handling ----------- //
+//05:
+
+int divCalc(int x, int y) {
+	try
+	{
+		if (y == 0)
+			throw runtime_error("Atempt dividing by zero");
+		else if (y < 0)
+			throw "error";
+		else
+			return x / y;
+
+	}
+	catch (exception& e)
+	{
+		cout << e.what();
+	}
+	catch (...)
+	{
+		cout << "Atempt dividing by negative";
+	}
+}
+
+
+
 
 int main()
 {
@@ -169,6 +195,14 @@ int main()
 	Bus bus(50, "bs123", "Id4456", 120, 1000, 200);
 	truck.display();
 	bus.display();*/
+
+
+
+	// -------- Exception Handling ----------- //
+	// //05:
+	int x = 5, y = 0;
+	divCalc(x, y);
+	
 
 
 
